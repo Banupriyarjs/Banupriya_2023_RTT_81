13af173bdedf7ad3fbe209c95f99535f0e1cdb84
banupriyarjs <banupriya.bubloo@gmail.com>
2023-09-19 14:21:16 -0400

    Scanner Class

---
+++ SecondProject/src/javaprograms/ScannerClass.java
@@ -0,0 +1,19 @@
+package javaprograms;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+
+	public static void main(String[] args) {
+	
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name=input.nextLine();
+		System.out.println("The entered name is :"+name);
+		input.close();
+		
+				;
+	}
+
+}
