d2846e13db110b4b9d176bb9e8d3eb9d2132fa87
banupriyarjs <banupriya.bubloo@gmail.com>
2023-09-20 15:42:56 -0400

    Updating CoffeeShop project

--- SecondProject/src/coffeeshop/CoffeeShop.java
+++ SecondProject/src/coffeeshop/CoffeeShop.java
@@ -3,10 +3,13 @@ package coffeeshop;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CoffeeShop {
 
 	List<Product> products = new ArrayList<>();
+	// this is the class member variable to make it easier
+	Scanner input = new Scanner(System.in);
 
 	public void setupProducts() {
 		Product coffee = new Product();
@@ -24,28 +27,17 @@ public class CoffeeShop {
 		cookie.setPrice(6.77);
 		products.add(cookie);
 
-		/*printProduct(coffee);
-		printProduct(tea);
-		printProduct(cookie);*/
-		printAllProducts();
+		Product donut = new Product();
+		donut.setName("Donut");
+		donut.setPrice(3.50);
+		products.add(donut);
+		/*
+		 * printProduct(coffee); printProduct(tea); printProduct(cookie);
+		 */
 
 	}
 
-	public void printProduct(Product product) {
-		System.out.println("Product Name : " + product.getName() + "  Price : " + product.getPrice());
-	}
-
-	public void printAllProducts() {
-		for (Product product : products) {
-			printProduct(product);
-		}
-	}
-
-	public static void main(String[] args) {
-
-		CoffeeShop cs = new CoffeeShop();
-		cs.setupProducts();
-
+	public void example() {
 		double coffee = 2.99d;
 		double expresso = 4.59d;
 		double cappuccino = 5.50d;
@@ -64,7 +56,42 @@ public class CoffeeShop {
 		DecimalFormat df = new DecimalFormat("$0.00");
 
 		System.out.println("Total Sales = " + df.format(totalSales));
+	}
 
+	public void printProduct(Product product) {
+		System.out.println("Product Name : " + product.getName() + "  Price : " + product.getPrice());
+	}
+
+	public void printAllProducts() {
+		for (Product product : products) {
+			printProduct(product);
+		}
+	}
+
+	public int userSelect() {
+		System.out.println("1. Print the menu items and prices");
+		System.out.println("2. Add an item to your order");
+		System.out.println("3. Print the items in your order.");
+		System.out.println("4. Checkout.");
+
+		System.out.println("\nWhat do you want to do?");
+
+		int select = input.nextInt();
+		return select;
+	}
+
+	public static void main(String[] args) {
+
+		CoffeeShop cs = new CoffeeShop();
+		cs.setupProducts();
+
+		int userSelection = cs.userSelect();
+
+		if (userSelection == 1) {
+			cs.printAllProducts();
+		} else {
+			System.out.println("User Input " + userSelection + " is unknnown.Try again");
+		}
 	}
 
 }
