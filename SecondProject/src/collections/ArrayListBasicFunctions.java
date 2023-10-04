6d8d326f3bc7d5f21dfbc0b5c2d1e32200cd9908
banupriyarjs <banupriya.bubloo@gmail.com>
2023-10-04 08:47:53 -0400

    Adding ArrayList and  ArrayListt Methods exaamples from GLAB 303.11.2 and 303.11.3

--- SecondProject/src/collections/ArrayListBasicFunctions.java
+++ SecondProject/src/collections/ArrayListBasicFunctions.java
@@ -5,27 +5,32 @@ import java.util.ArrayList;
 public class ArrayListBasicFunctions {
 
 	public static void main(String[] args) {
-		ArrayList<String> mylist = new ArrayList<String>();
-		mylist.add("New Jersey");
-		mylist.add("Dallas");
-		mylist.add("New York");
-		mylist.add("Chicago");
-		mylist.add("LA");
-		
-		System.out.println(mylist.toString());
-		System.out.println("\nReturn boolean :" + mylist.contains("Chicago"));
-		System.out.println("\nReturn index value of Element :" + mylist.indexOf("Chicago"));
-		System.out.println("\nReturns the actual Element at the given index :" + mylist.get(2));
-		System.out.println("\nReturns the length of the arraylist :" + mylist.size());
+		ArrayList<String> myList = new ArrayList<String>();
+		myList.add("New Jersey");
+		myList.add("Dallas");
+		myList.add("New York");
+		myList.add("Chicago");
+		myList.add("LA");
+
+		System.out.println(myList.toString());
+
+		for (String x : myList) {
+			System.out.println(x);
+		}
+		System.out.println("\nReturn boolean :" + myList.contains("Chicago"));
+		System.out.println("\nReturn index value of Element :" + myList.indexOf("Chicago"));
+		System.out.println("\nReturns the actual Element at the given index :" + myList.get(2));
+		System.out.println("\nReturns the length of the arraylist :" + myList.size());
 		// Remove the element at the given index. Returns the removed element.
 		// remove element at index 3
-		String removedElement = mylist.remove(3);
+		String removedElement = myList.remove(3);
 		System.out.println("\nRemoved Element: " + removedElement);
 		// remove all elements
-		mylist.clear();
-		System.out.println("\nArrayList after clear(): " + mylist);
-		
-		System.out.println(mylist.toString());
+		myList.clear();
+		System.out.println("\nArrayList after clear(): " + myList);
+
+		System.out.println(myList.toString());
+
 	}
 
 }
