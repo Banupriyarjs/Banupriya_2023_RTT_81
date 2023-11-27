8d770f2fe26086c76e91e4e7c3299a071e4582d3
banupriyarjs <banupriya.bubloo@gmail.com>
2023-11-27 15:49:02 -0500

    Homework - Creating employee entities and save the records to the database. Done

---
+++ springboot/src/main/java/org/perscholas/springboot/database/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package org.perscholas.springboot.database.dao;
+
+import org.perscholas.springboot.database.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeDAO extends JpaRepository<Employee,Long>
+{
+    public Employee findById(int id);
+}
