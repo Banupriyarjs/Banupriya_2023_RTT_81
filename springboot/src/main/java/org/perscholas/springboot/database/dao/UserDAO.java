dd8a1fc55e1e6b010502b218c770b894b44cfbc3
banupriyarjs <banupriya.bubloo@gmail.com>
2023-12-06 00:26:09 -0500

    Added AuthController, RegisterUserFormBean, UserService, and register.jsp pages to enable the functionality of registering user

--- springboot/src/main/java/org/perscholas/springboot/database/dao/UserDAO.java
+++ springboot/src/main/java/org/perscholas/springboot/database/dao/UserDAO.java
@@ -1,7 +1,11 @@
 package org.perscholas.springboot.database.dao;
 
+import org.perscholas.springboot.database.entity.Customer;
 import org.perscholas.springboot.database.entity.Employee;
+import org.perscholas.springboot.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDAO extends JpaRepository<Employee,Long> {
+public interface UserDAO extends JpaRepository<User,Long> {
+
+    public User findById(Integer id);
 }
---
+++ springboot/src/main/java/org/perscholas/springboot/formbean/RegisterUserFormBean.java
@@ -0,0 +1,27 @@
+package org.perscholas.springboot.formbean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class RegisterUserFormBean {
+
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @Length(min=8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Password must contain at least one lowercase letter, one uppercase letter, and one digit")
+    private String password;
+
+    @NotEmpty(message = "Confirm Password cannot be empty")
+    private String confirmPassword;
+}
